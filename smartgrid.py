91e23aa9a8aaf0f09261cbdb4cef0a436f804931
koffiehuis <markmalestein@gmail.com>
2018-11-08 13:50:37 +0100

    colourlist

--- smartgrid.py
+++ smartgrid.py
@@ -1,3 +1,4 @@
+<<<<<<< current
 from house import House
 from battery import Battery
 import csv
@@ -98,7 +99,8 @@ class Smartgrid(object):
                 y = coordinates.split(",", 1)[1]
                 x = re.sub("\D", "", x)
                 y = re.sub("\D", "", y)
-                colour = self.colour_list[id]
+                # colour = smart.colour_list[id]
+                print(smart.colour_list)
                 batteries[id] = Battery(cap, x, y, colour)
 
         # return dict to INIT
@@ -175,3 +177,156 @@ if __name__ == "__main__":
 
     smart.link_houses()
     smart.plot_houses()
+=======
+from house import House
+from battery import Battery
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.ticker as ticker
+import re
+import operator
+# Dit moet later worden gesoftcodedet
+INPUT_HOUSES = "wijk1_huizen.csv"
+INPUT_BATTERIES = "wijk1_batterijen.txt"
+
+class Smartgrid(object):
+    def __init__(self):
+        self.houses = self.load_houses(INPUT_HOUSES)
+        self.batteries = self.load_batteries(INPUT_BATTERIES)
+
+    def load_houses(self, map):
+        # open file
+        with open(f"Huizen&Batterijen/{INPUT_HOUSES}", newline="") as houses_csv:
+            # read data from csv
+            data_houses = csv.reader(houses_csv, delimiter=",")
+            # skip headers
+            next(data_houses, None)
+            houses = {}
+
+            # for every house, save coordinates and output in dictionary
+            # name for instance in dict is Xcoord-Ycoord
+            for row in data_houses:
+                x = row[0]
+                y = row[1]
+                id = f"{x}-{y}"
+
+                output = row[2]
+                houses[id] = House(x, y, output)
+        # returns dict, goes to init (self.houses)
+        return houses
+
+    def plot_houses(self):
+
+        x_houses, y_houses, x_batt, y_batt  = smart.get_coordinates()
+
+        # make plot
+        ax = plt.gca()
+        ax.axis([-2, 52, -2 , 52])
+        ax.scatter(x_houses , y_houses, marker = ".")
+        ax.scatter(x_batt, y_batt, marker = "o")
+        ax.set_xticks(np.arange(0, 52, 1), minor = True)
+        ax.set_yticks(np.arange(0, 52, 1), minor = True)
+        ax.grid(b = True, which="major", linewidth=1)
+        ax.grid(b = True, which="minor", linewidth=.2)
+
+
+
+        plt.show()
+
+    def load_batteries(self, map):
+        with open(f"Huizen&Batterijen/{INPUT_BATTERIES}") as batteries_text:
+
+            # read text file per line
+            data_batteries = batteries_text.readlines()
+
+            # delete headers
+            data_batteries.pop(0)
+
+            batteries = {}
+
+            # for every batterie isolate coordinates and capacity
+            for id, battery in enumerate(data_batteries):
+                coordinates = battery.split("\t", 1)[0]
+                cap = battery.split("\t", 1)[1]
+                cap = cap.strip()
+                x = coordinates.split(",", 1)[0]
+                y = coordinates.split(",", 1)[1]
+                x = re.sub("\D", "", x)
+                y = re.sub("\D", "", y)
+                batteries[id] = Battery(cap, x, y)
+
+        # return dict to INIT
+        return batteries
+
+    def link_houses(self):
+        # order the batteries for each house
+        for house in list(smart.houses.values()):
+            dist = house.dist
+            ord_dist = sorted(dist.items(), key=operator.itemgetter(1))
+
+            # for right now, the link is the shortest
+            # regardless of battery capacity
+            house.link = ord_dist[0]
+            house.ord_dist = list(ord_dist)
+
+
+    def calculate_cable(self):
+        # get coordinates
+        x_houses, y_houses, x_batt, y_batt  = smart.get_coordinates()
+
+        all_diff = []
+        for x_house, y_house in list(zip(x_houses, y_houses)):
+            house_diff = {}
+            counter = 0
+            for x, y in list(zip(x_batt, y_batt)):
+                x_diff = abs(x - x_house)
+                y_diff = abs(y - y_house)
+                house_diff[counter] = (x_diff + y_diff)
+                counter += 1
+            all_diff.append(house_diff)
+
+        # set as attributes
+        keys_list = list(smart.houses.keys())
+        for i, key in enumerate(keys_list):
+                smart.houses[key].dist = all_diff[i]
+
+        #################################################################
+        # DUIDELIJK AANGEVEN DAT DIT ALLEEN VOOR PYTHON 3.6+ WERKT
+        #################################################################3
+
+    def get_coordinates(self):
+        x_houses = []
+        y_houses = []
+
+        x_batt = []
+        y_batt = []
+
+        # turn dict to list so we can iterate through
+        houses_list = list(smart.houses.values())
+        batteries_list = list(smart.batteries.values())
+
+        # for every house save coordinates to lists
+        for house in houses_list:
+            x_houses.append(house.x)
+            y_houses.append(house.y)
+
+        # for every battery save coordinates to lists
+        for battery in batteries_list:
+            x_batt.append(battery.x)
+            y_batt.append(battery.y)
+
+        return x_houses, y_houses, x_batt, y_batt
+
+    # def calculate_battery_input():
+    #     # TODO
+    #
+    # def calculate_battery_output():
+    #     # TODO
+    #     dict = {}
+if __name__ == "__main__":
+    smart = Smartgrid()
+    #smart.plot_houses(smart.houses, smart.batteries)
+    smart.calculate_cable()
+    smart.link_houses()
+>>>>>>> before discard
