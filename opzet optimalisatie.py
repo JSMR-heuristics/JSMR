6c8c693f5df125074cd420ff0bde2d486aabcc7c
Sebastiaan Schneider <sebastiaanschneider@gmail.com>
2018-11-08 14:57:59 +0100

    optimize 0.03

--- opzet optimalisatie.py
+++ opzet optimalisatie.py
@@ -7,17 +7,22 @@ for battery in self.batteries:
         battery.full = True
 
 switch = 9999
+switch_batt = 0
 go = 9999
+go_batt = 0
+changer = 0
 
 for battery in self.batteries:
     while battery.full == True:
         for house in battery.linked_houses:
             for link in house.diffs.items():
-                if self.batteries[link[0]].full == False && link[1] < switch:
+                if (battery.capacity - sum(battery.linked_houses.output)) >
+                house.output && link[1] < switch:
                     switch = link[1]
                     switch_batt = link[0]
             if switch < go:
                 go = switch
                 go_batt = switch_batt
                 changer = house
+                switch = 9999
         changer.link = go_batt
