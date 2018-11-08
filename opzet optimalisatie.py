6c29706794b12f8033bd12a7c0152dbc60245453
Sebastiaan Schneider <sebastiaanschneider@gmail.com>
2018-11-08 14:00:46 +0100

    optimize 0.02

--- opzet optimalisatie.py
+++ opzet optimalisatie.py
@@ -1,10 +1,6 @@
-kijken naar de huizen op alle volle Batterijen
-kies het huis dat het goedkoopst naar een niet volle batterij kan
-verander de link van dat huis naar die niet volle batterij
-
-
-# change = lijst van huizen met de verschillen in afstand tussen het huis en \
-# de eerste batterij en de andere batterijen (0-1, 0-2, 0-3, 0-4)
+# kijken naar de huizen op alle volle Batterijen
+# kies het huis dat het goedkoopst naar een niet volle batterij kan
+# verander de link van dat huis naar die niet volle batterij
 
 for battery in self.batteries:
     if battery.capacity <= sum(battery.linked_houses.output):
