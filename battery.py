37e27f7b5ba0bc2c2c2394b68f9581b81279f626
Sebastiaan Schneider <sebastiaanschneider@gmail.com>
2018-11-09 15:06:12 +0100

    optimize 0.2

--- battery.py
+++ battery.py
@@ -1,13 +1,32 @@
 class Battery(object):
 
     def __init__(self, capacity, x, y, colour):
-        self.capacity = capacity
-        self.full = False
+        self.capacity = float(capacity)
         self.x = int(x)
         self.y = int(y)
         self.colour = colour
         self.linked_houses = []
         # latere opdrachten de prijs
+
+
+    def full(self):
+        sum = 0.00
+        for j in self.linked_houses:
+            sum += j.output
+        if self.capacity < sum:
+            return True
+        else:
+            return False
+
+
+    def filled(self):
+        sum = 0.00
+        for j in self.linked_houses:
+            sum += j.output
+        return sum
+
+
+
 
 
     # Later in de opdracht functies als add_battery en move battery er bij
