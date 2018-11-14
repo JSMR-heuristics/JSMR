ea56bfd351b4ae7a7b0abd61f1a9592fd818f2ea
koffiehuis <markmalestein@gmail.com>
2018-11-14 10:31:34 +0100

    Algoritme doet het nu voor alle wijken

--- smartgrid.py
+++ smartgrid.py
@@ -199,84 +199,97 @@ class Smartgrid(object):
         return x_houses, y_houses, x_batt, y_batt
 
     def optimize(self):
+        """
+        This function changes links between houses and batteries
+        so no battery is over it's capacity, this will be done
+        with lowest cost possible for this algorithm
+        """
+        # Initialize changes counter, this gives insight to
+        # the speed of this algorithm
+        changes = 0
 
-      # Check initial status
-      for i in self.batteries:
-          print(self.batteries[i].full())
-      for i in self.batteries:
-          print(self.batteries[i].filled())
+        # While one or more batteries are over their capacity
+        while self.check_full():
 
-      # Initialize variables
-      switch = 9999
-      go = 9999
-      changes = 0
+            # Sorts batteries based off total inputs from high to low
+            total_inputs = []
+            for battery in self.batteries.values():
+                total_inputs.append([battery.filled(), battery])
+            high_low = sorted(total_inputs, key=operator.itemgetter(0), reverse = True)
 
-      # Between nope and yep is the range where it'll be hard to find a
-      # decent house to add to a battery
-      nope = 36
-      yep = 8
+            # Prioritize battery with highest inputs
+            # to disconnect a battery from
+            for i in high_low:
+                battery = i[1]
+                distance_list = []
 
-# conditie toevoegen om te zorgen dat huizen niet op een batterij komen die verder dan een max afstand ligt
-# conditie toevoegen om te zorgen dat een huis niet wordt verplaatst als dat de batterij nét niet onder full brengt
+                # Sort houses linked to this battery by distance
+                # to other battery from low to high
+                for house in battery.linked_houses:
+                    element = []
+                    batts = list(house.diffs.keys())
+                    distance = list(house.diffs.values())
+                    houses = [house] * len(distance)
+                    outputs = [house.output] * len(distance)
+                    element = list(map(list, zip(batts, distance, houses, outputs)))
+                    distance_list += element
+                distance_list = sorted(distance_list, key=operator.itemgetter(1))
 
-      # Keep looping until all batteries are below max capacity
-      while self.batteries[0].full() or self.batteries[1].full() or self.batteries[2].full() or self.batteries[3].full() or self.batteries[4].full():
-          # Iterate over every battery
-          for i in self.batteries:
-              print(f"i = {i}")
-              # Keep moving houses until the battery is no longer full
-              while self.batteries[i].full():
-                  nope_list = []
-                  # Iterate every house linked to the battery
-                  for house in self.batteries[i].linked_houses:
-                      # Check every possible connection the house has
-                      for link in house.diffs.items():
-                          max_cap = self.batteries[link[0]].capacity
-                          cur_cap = self.batteries[link[0]].filled()
-                          cur_input = house.output
-                          # d = leftover capacity minus the house that will be added
-                          rest_cap = (max_cap - cur_cap) - cur_input
-                          # If the switch is smaller than the other links
-                          # of the house, consider it for switching
-                          if link[1] < switch and not (f"{house}, {link}" in nope_list):
-                              # If the connection switch is possible, save it,
-                              # otherwise add it to a list of houses-connections
-                              # that should be ignored. If adding the houses
-                              # puts the battery in an impractical range of
-                              # capacity, also ignore the switch
-                              if (max_cap < cur_cap + cur_input
-                                 or ((rest_cap < nope) and (rest_cap > yep))):
-                                  nope_list.append(f"{house}, {link}")
-                              else:
-                                  switch = link[1]
-                                  switch_batt = link[0]
-                      # Check the house's best switch option against the best
-                      # overal option for the battery so far
-                      if switch < go:
-                          go = switch
-                          go_batt = switch_batt
-                          changer = house
-                          switch = 9999
-                  # The loop has checked every house now, so it changes the
-                  # connection for the best house option
-                  self.swap_houses(changer, self.batteries[i],
-                                   self.batteries[go_batt], changes)
-                  changes += 1
-                  print(f"Current changes = {changes}")
-                  go = 9999
-          # Check results
-          for i in self.batteries:
-              print(self.batteries[i].full())
-          for i in self.batteries:
-              print(self.batteries[i].filled())
+                # Determine the cheapest option first, if any
+                # else transfer option with lowest output
+                try:
+                    house, to_batt = self.find_best(distance_list, "strict")
+                except TypeError:
+                    house, to_batt = self.find_best(distance_list, "not-strict")
+
+                # Switch the house from battery
+                curr_batt = house.link
+                changes += 1
+                self.swap_houses(house, curr_batt, to_batt, changes)
+
+
+    def check_full(self):
+        """
+        Returns True if one or more of the batteries is over it's
+        capacity, False if not.
+        """
+        switch = False
+        for battery in self.batteries.values():
+            if battery.full() is True:
+                switch = True
+
+        return switch
+
+    def find_best(self, list, status):
+        """
+        Tries to find either the cheapest house to possibly switch from battery
+        or the one with the lowest possible output
+        """
+        if status is "strict":
+            for option in list:
+                if self.batteries[option[0]].filled() + option[2].output <= self.batteries[option[0]].capacity:
+                    return option[2], self.batteries[option[0]]
+        else:
+            list = sorted(list, key=operator.itemgetter(3))
+            for option in list:
+                if (option[2].link.filled() - option[2].output) < option[2].link.capacity:
+                    print(option[2].link.filled() - option[2].output)
+                    return option[2], self.batteries[option[0]]
+
 
     def swap_houses(self, house, current_batt, next_batt, changes):
-          house.link = next_batt
-          next_batt.linked_houses.append(house)
-          current_batt.linked_houses.remove(house)
-          print(f"house at x{house.x}/y{house.y} changed from battery at x{current_batt.x}/y{current_batt.y} to battery at x{next_batt.x}/y{next_batt.y}")
-          print(f"house capacity = {house.output}")
-          print(f"capacity = {current_batt.filled()}")
+        """
+        Switches house from battery it's currently linked to, to the next
+        one
+        """
+        house.link = next_batt
+        next_batt.linked_houses.append(house)
+        current_batt.linked_houses.remove(house)
+        print(f"house at x{house.x}/y{house.y} changed from battery at x{current_batt.x}/y{current_batt.y} to battery at x{next_batt.x}/y{next_batt.y}")
+        print(f"house capacity = {house.output}")
+        print(f"capacity = {current_batt.filled()}")
+        print(f"changes = {changes}")
+
 
 
 if __name__ == "__main__":
