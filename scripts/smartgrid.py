3d488a0d0acd2d7c5544c148580e7b25e40aa5e2
Goblok0 <43988726+Goblok0@users.noreply.github.com>
2018-11-16 13:58:45 +0100

    -

--- scripts/smartgrid.py
+++ scripts/smartgrid.py
@@ -13,16 +13,15 @@ import os
 
 from pathlib import Path
 
-<<<<<<< HEAD
 # Moet worden vervangen door user input
 
 INPUT = 3
 
-=======
+
 # nog aanpassen als we meerdere algoritmes en/of eigen wijken gaan maken
 # en voor tussenplots, die maken het algorimte een stuk slomer
 # Validates user input and gives instructions if it's wrong
-plot = False
+plot = True
 
 if len(sys.argv) not in [2, 3]:
         print("Usage: python smargrid.py <wijknummer> <plot>\nwijknummer should be 1,2 or 3")
@@ -41,7 +40,6 @@ elif len(sys.argv) is 3:
     else:
         INPUT = sys.argv[1]
         plot = True
->>>>>>> da9ecd367caeb6915324511c839ec12d53809eec
 
 class Smartgrid(object):
     def __init__(self):
@@ -171,7 +169,9 @@ class Smartgrid(object):
         #     plt.text(x, y, f"{count}")
         #     count += 1
         # plt.show()
-        plt.savefig(f'plot{changes}.png')
+        subpath = "figures"
+        path = str(Path.cwd()).replace("scripts", subpath)
+        plt.savefig(path + f'plot{changes}.png', dpi = 300)
 
 
     def link_houses(self):
