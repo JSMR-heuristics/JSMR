af605626b465c77b19cac20b0c05310c15e9327d
Goblok0 <43988726+Goblok0@users.noreply.github.com>
2018-11-19 13:00:31 +0100

    -

--- scripts/house.py
+++ scripts/house.py
@@ -7,22 +7,9 @@ class House(object):
         # output of the house instance
         self.output = float(output)
 
-
         # to which battery is the house currently connected
         self.link = []
-
 
         # the differences in distance between the closest Battery
         # and the other batteries
         self.diffs = {}
-
-    # def add_distance(self, dist_list):
-    #     self.dist = dist_list
-    # def add_house(self, ""):
-    #     Smartgrid.houses["id"] = .....
-    #
-    # def move_house(self, id, to_coordinates):
-    #     self.house[self.id].coordinates = self.to_coordinates
-    #
-    # def delete_house(self, id):
-    #     del self.houses[self.id]
