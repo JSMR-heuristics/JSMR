1e23c3af552cc1c137740b282eb93416c57b6e5e
Goblok0 <43988726+Goblok0@users.noreply.github.com>
2018-11-23 14:27:34 +0100

    fixed class imports

--- code/algorithms/smartgrid.py
+++ code/algorithms/smartgrid.py
@@ -16,9 +16,8 @@ from helpers import *
 
 path = str(Path.cwd()).replace("algorithms", "classes")
 sys.path.append(path)
-import battery as battery
-import house as house
-
+from battery import Battery
+from house import House
 # nog aanpassen als we meerdere algoritmes en/of eigen wijken gaan maken
 # en voor tussenplots, die maken het algorimte een stuk slomer
 # Validates user input and gives instructions if it's wrong
@@ -59,8 +58,11 @@ class Smartgrid(object):
         objects. Returns instances in dict to __init__
         """
         # find specific directory with the data
-        subpath = f"data\wijk{INPUT}_huizen.csv"
-        path = str(Path.cwd()).replace("scripts", subpath)
+        subpath = f"data/wijk{INPUT}_huizen.csv"
+        path_mand = str(Path.cwd()).replace("algorithms", "")
+        path = str(path_mand.replace("code", subpath))
+        print("meh")
+        print(path)
         # open file
         with open(path, newline="") as houses_csv:
 
@@ -90,8 +92,9 @@ class Smartgrid(object):
         objects. Returns instances in dict to __init__
         """
         # find specific directory with the data
-        subpath = f"data\wijk{INPUT}_batterijen.txt"
-        path = str(Path.cwd()).replace("scripts", subpath)
+        subpath = f"data/wijk{INPUT}_batterijen.txt"
+        path_mand = str(Path.cwd()).replace("algorithms", "")
+        path = str(path_mand.replace("code", subpath))
 
         with open(path) as batteries_text:
 
